711a6cd69f8f2e306dde531121a53a106e5ae693
Mellemy <emppuli2003@gmail.com>
2025-07-01 23:19:30 +0300

    4.3 ja 4.4

---
+++ osa4/blogilista/utils/list_helper.js
@@ -0,0 +1,23 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+ 
+}
+
+const mostBlogs = (blogs) => {
+
+}
+
+const mostLikes = (blogs) => {
+
+}
+
+module.exports = {
+  dummy, totalLikes
+}
