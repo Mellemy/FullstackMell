7ba10f950556f188dce67aa5ef74e2a423117d94
Mellemy <emppuli2003@gmail.com>
2025-07-02 00:15:47 +0300

    4.7done

--- osa4/blogilista/tests/first.test.js
+++ osa4/blogilista/tests/first.test.js
@@ -17,12 +17,117 @@ describe('total likes', () => {
       author: 'Edsger W. Dijkstra',
       url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
       likes: 5,
-      __v: 0
+      __v: 0,
     }
+    
   ]
-
   test('when list has only one blog equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     assert.strictEqual(result, 5)
   })
+  test('list has more than one blog', () => {
+  const manyblogs = [
+   {
+      title: 'Big blogger party',
+      author: 'John Testman',
+      likes: 7
+    },
+    {
+      title: 'Small blogger party',
+      author: 'John Testman',
+      likes: 7
+    },
+  ]
+  const result = listHelper.totalLikes(manyblogs)
+  assert.strictEqual(result, 14)
+})})
+
+describe('favorite blog', () => {
+  const blogs = [
+    {
+      title: 'Big blogger party',
+      author: 'John Testman',
+      likes: 7
+    },
+    {
+      title: 'Small blogger party',
+      author: 'John Testman',
+      likes: 7
+    },
+    {
+      title: 'Another blog',
+      author: 'Mary',
+      likes: 3
+    }
+  ]
+
+  test('return most liked blog', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(result, {
+      title: 'Big blogger party',
+      author: 'John Testman',
+      likes: 7
+    })
+  })
 })
+
+describe('most blogs', () => {
+  const blogs = [
+    {
+      title: 'Big blogger party',
+      author: 'John Testman',
+      likes: 7
+    },
+    {
+      title: 'Small blogger party',
+      author: 'John Testman',
+      likes: 7
+    },
+    {
+      title: 'Another blog',
+      author: 'Mary',
+      likes: 3
+    }
+  ]
+  test('returns author with the most blogs', () => {
+    const result = listHelper.mostBlogs(blogs)
+    assert.deepStrictEqual(result, {
+      author: 'John Testman',
+      blogs: 2
+    })
+  })
+})
+
+describe('most likes', () => {
+  const blogs = [
+  {
+      title: 'Big blogger party',
+      author: 'John Testman',
+      likes: 8
+    },
+    {
+      title: 'Small blogger party',
+      author: 'John Testman',
+      likes: 7
+    },
+    {
+      title: 'Another blog',
+      author: 'Mary',
+      likes: 3
+    }
+     ,{
+      title: 'Another blog2',
+      author: 'Mary',
+      likes: 7
+    }
+  ]
+
+  test('blogger with most likes', () => {
+    const result = listHelper.mostLikes(blogs)
+    assert.deepStrictEqual(result, {
+      author: 'John Testman',
+      likes: 15
+    })
+  })
+})
+
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -7,17 +9,39 @@ const totalLikes = (blogs) => {
 }
 
 const favoriteBlog = (blogs) => {
- 
+   if (blogs.length === 0) return null
+
+  return blogs.reduce((fav, blog) => {
+  return blog.likes > fav.likes ? blog : fav
+  })
 }
 
 const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
 
+  const grouped = _.groupBy(blogs, 'author')
+  const topBlogger = _.maxBy(Object.keys(grouped), author => grouped[author].length)
+
+  return {
+    author: topBlogger,
+    blogs: grouped[topBlogger].length
+  }
 }
 
 const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null
 
+  const topBlogger2 = _(blogs)
+    .groupBy('author')
+    .map((posts, author) => ({
+      author,
+      likes: _.sumBy(posts, 'likes')
+    }))
+    .maxBy('likes')
+
+  return topBlogger2
 }
 
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
