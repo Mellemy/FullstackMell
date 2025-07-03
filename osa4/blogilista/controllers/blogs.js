5d0238218d5ae3dfb9ec59c5d26e9531108d9350
Mellemy <emppuli2003@gmail.com>
2025-07-03 04:23:18 +0300

    Supertests done

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -9,9 +9,53 @@ blogsRouter.get('/', async (req, res) => {
 })
 
 blogsRouter.post('/', async (req, res) => {
-  const blog = new Blog(req.body)
+  const { title, author, url, likes } = req.body
+
+    if (!title || !url) {
+    return res.status(400).json({ error: 'add title and url' })
+  }
+    const blog = new Blog({
+    title, author, url,
+    likes: likes || 0
+  })
   const savedBlog = await blog.save()
   res.status(201).json(savedBlog)
 })
+
+blogsRouter.delete('/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const result = await Blog.findByIdAndDelete(id)
+
+    if (!result) {
+      return res.status(404).json({ error: 'blog not found' })
+    }
+    res.status(204).end()
+  } catch (error) {
+    res.status(400).json({ error: 'malformatted id' })
+  }
+})
+
+blogsRouter.put('/:id', async (req, res) => {
+  const { title, author, url, likes } = req.body
+
+  const updatedBlog = {
+    title, author, url, likes
+  }
+  try {
+    const result = await Blog.findByIdAndUpdate(req.params.id, updatedBlog, {
+      new: true, runValidators: true
+    })
+
+    if (!result) {
+      return res.status(404).json({ error: 'blog not found' })
+    }
+    res.json(result)
+  } catch (error) {
+    res.status(400).json({ error: 'malformatted id' })
+  }
+})
+
 
 module.exports = blogsRouter
---
+++ osa4/blogilista/tests/supertests.test.js
@@ -0,0 +1,142 @@
+const { test, beforeEach, after } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const BlogTables = [
+  {
+      title: 'Big blogger party',
+      author: 'John Testman',
+      likes: 7
+    },
+    {
+      title: 'Small blogger party',
+      author: 'John Testman',
+      likes: 7
+    },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (const blog of BlogTables) {
+    const blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('Return correct amount of Json', async () => {
+ const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+     require('node:assert').strictEqual(response.body.length, BlogTables.length)
+})
+
+test('blogs have ID', async () => {
+  const response = await api.get('/api/blogs')
+  const blog = response.body[0]
+  require('node:assert').strictEqual(blog.id !== undefined, true)
+})
+
+test('POST increase by 1', async () => {
+  const PostedBlog = {
+    title: 'Marys new blog',
+    author: 'Mary',
+    url: 'http://IDK.com/testing',
+    likes: 2
+  }
+  await api
+    .post('/api/blogs')
+    .send(PostedBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  require('node:assert').strictEqual(response.body.length, BlogTables.length + 1)
+  const titles = response.body.map(blog => blog.title)
+  require('node:assert').strictEqual(titles.includes('Marys new blog'), true)
+})
+
+test('0 for no likes', async () => {
+const PostedBlog2 = {
+    title: 'Marys new blog',
+    author: 'Mary',
+    url: 'http://IDK.com/testing'
+  }
+  const response = await api
+    .post('/api/blogs')
+    .send(PostedBlog2)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  require('node:assert').strictEqual(response.body.likes, 0)
+})
+
+
+test('no title returns 400', async () => {
+ const PostedBlog3 = {
+    author: 'Mary',
+    url: 'http://IDK.com/testing',
+    likes: 2
+  }
+  await api
+    .post('/api/blogs')
+    .send(PostedBlog3)
+    .expect(400)
+})
+
+test('no url returns 400', async () => {
+ const PostedBlog4 = {
+    title: 'Marys new blog',
+    author: 'Mary',
+    likes: 2
+  }
+  await api
+    .post('/api/blogs')
+    .send(PostedBlog4)
+    .expect(400)
+})
+
+test('blog deletion', async () => {
+  const blogsAtStart = await api.get('/api/blogs')
+  const Deleted = blogsAtStart.body[0]
+
+  await api
+    .delete(`/api/blogs/${Deleted.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await api.get('/api/blogs')
+  const ids = blogsAtEnd.body.map(b => b.id)
+
+  require('node:assert').strictEqual(
+    blogsAtEnd.body.length,
+    blogsAtStart.body.length - 1
+  )
+
+  require('node:assert').strictEqual(ids.includes(Deleted.id), false)
+})
+
+
+test('blog update', async () => {
+  const blogsAtStart = await api.get('/api/blogs')
+  const Updated = blogsAtStart.body[0]
+
+const NewLikes = { likes: 12 }
+
+ const response = await api
+    .put(`/api/blogs/${Updated.id}`)
+    .send(NewLikes)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  require('node:assert').strictEqual(response.body.likes, 12)
+})
+
+
+after(async () => {
+  await mongoose.connection.close()
+})
