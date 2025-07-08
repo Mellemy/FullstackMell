1236a22de57e3b28c748967e4a915f68e78ff4c8
Mellemy <emppuli2003@gmail.com>
2025-07-08 19:40:40 +0300

    tehtävä 5.1 ja 5.2

--- osa5/bloglist-frontend-main/src/App.jsx
+++ osa5/bloglist-frontend-main/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+   const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+    const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +15,85 @@ const App = () => {
     )  
   }, [])
 
+    useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async  (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+     try {
+      const user = await loginService.login({
+        username, password,
+      })
+      
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+        blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+    const Logout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+    blogService.setToken(null)
+  }
+
+    if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+         <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+      </div>
+    )
+  }
   return (
     <div>
       <h2>blogs</h2>
+       <p>{user.name} logged in
+        <button onClick={Logout} >Logout</button>
+       </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
   )
+  
+
 }
 
 export default App
