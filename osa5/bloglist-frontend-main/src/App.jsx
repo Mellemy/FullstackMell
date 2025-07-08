8a7de882af50fc3ddbfac3df473db606328bc4f4
Mellemy <emppuli2003@gmail.com>
2025-07-08 20:09:11 +0300

    5.3 ja 5.4

--- osa5/bloglist-frontend-main/src/App.jsx
+++ osa5/bloglist-frontend-main/src/App.jsx
@@ -2,12 +2,19 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import './index.css'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
    const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
     const [user, setUser] = useState(null)
+    const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+    const [newUrl, setNewUrl] = useState('')
+    const [errorMessage, setErrorMessage] = useState(null)
+const [successMessage, setSuccessMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -40,10 +47,10 @@ const App = () => {
       setUsername('')
       setPassword('')
     } catch (exception) {
-      setErrorMessage('wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+    setErrorMessage('wrong username or password')
+    setTimeout(() => {
+    setErrorMessage(null)
+    }, 5000)
     }
   }
 
@@ -53,10 +60,42 @@ const App = () => {
     blogService.setToken(null)
   }
 
+  const CreateBlog = async (event) => {
+  event.preventDefault()
+  if (!newTitle || !newAuthor || !newUrl) {
+    setErrorMessage('please fill all fields')
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+    return
+  }
+  try {
+    const newBlog = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl,
+    }
+
+    const createdBlog = await blogService.create(newBlog)
+    setBlogs(blogs.concat(createdBlog))
+    setNewTitle('')
+    setNewAuthor('')
+    setNewUrl('')
+    setSuccessMessage(`a new blog "${createdBlog.title}" was added by ${user.name}`)
+    setTimeout(() => {
+    setSuccessMessage(null)
+    } , 5000)
+  } catch (error) {
+    console.error('Failed to create blog', error)
+  }
+}
+
     if (user === null) {
     return (
       <div>
+        
         <h2>Log in to application</h2>
+         {errorMessage && <div className="error">{errorMessage}</div>}
          <form onSubmit={handleLogin}>
         <div>
           username
@@ -78,19 +117,44 @@ const App = () => {
         </div>
         <button type="submit">login</button>
       </form>
+      
       </div>
     )
   }
   return (
     <div>
-      <h2>blogs</h2>
+    
+      <h2>Blogs</h2>
+         {errorMessage && <div className="error">{errorMessage}</div>}
+        {successMessage && <div className="success">{successMessage}</div>}
        <p>{user.name} logged in
         <button onClick={Logout} >Logout</button>
        </p>
+      
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+
+    <h2>Create a new blog</h2>
+    <form onSubmit={CreateBlog}>
+    <div>
+    Title:
+    <input type="text"  value={newTitle} onChange={({ target }) => setNewTitle(target.value)} />
+  </div>
+  <div>
+    Author:
+    <input type="text"  value={newAuthor} onChange={({ target }) => setNewAuthor(target.value)}  />
+  </div>
+  <div>
+    URL:
+    <input type="text" value={newUrl}
+      onChange={({ target }) => setNewUrl(target.value)}
+    />
+  </div>
+  <button type="submit">Create</button>
+</form>
     </div>
+    
   )
   
 
