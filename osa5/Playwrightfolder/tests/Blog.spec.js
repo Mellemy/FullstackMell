9a3a10cd80c9b9988d5e0164c1cdfda91c6401b3
Mellemy <emppuli2003@gmail.com>
2025-07-10 07:48:33 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- osa5/Playwrightfolder/tests/Blog.spec.js
+++ osa5/Playwrightfolder/tests/Blog.spec.js
@@ -1,41 +1,32 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-
+test.describe.configure({ mode: 'serial' })
 describe('Blog app', () => {
-  beforeEach(async ({ page, request }) => {
-   
-    await request.post('http://localhost:3003/api/testing/reset')
-    await request.post('http://localhost:3003/api/users', {
-      data: {
-        name: 'Matti Luukkainen',
-        username: 'mluukkai',
-        password: 'salainen'
-      }
-    })
+ beforeEach(async ({ page, request }) => {
+  await request.post('http://localhost:3003/api/testing/reset')
 
-    await page.goto('http://localhost:5173')
+  await request.post('http://localhost:3003/api/users', {
+    data: {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
   })
+  await request.post('http://localhost:3003/api/users', {
+    data: {
+      name: 'John Testman',
+      username: 'Johnny',
+      password: 'secret!'
+    }
+  })
+
+  await page.goto('http://localhost:5173')
+})
   test('Login form is shown', async ({ page }) => {
 
      const locator = await page.getByText('Log in to application')
   await expect(locator).toBeVisible()
   })
- })
 
-
-   describe('Login', () => {
-    beforeEach(async ({ page, request }) => {
-   
-    await request.post('http://localhost:3003/api/testing/reset')
-    await request.post('http://localhost:3003/api/users', {
-      data: {
-        name: 'Matti Luukkainen',
-        username: 'mluukkai',
-        password: 'salainen'
-      }
-    })
-
-    await page.goto('http://localhost:5173')
-  })
     test('succeeds with correct credentials', async ({ page }) => {
 
     await page.getByRole('textbox').first().fill('mluukkai')
@@ -70,7 +61,7 @@ describe('Blog app', () => {
 
   })
 
-test.only('Liking a blog', async ({ page }) => {
+test('Liking a blog', async ({ page }) => {
   await page.getByRole('textbox').first().fill('mluukkai')
   await page.getByRole('textbox').last().fill('salainen')
   await page.getByRole('button', { name: 'login' }).click()
@@ -91,4 +82,50 @@ test.only('Liking a blog', async ({ page }) => {
   await expect(likes).toContainText('Likes: 1')
 })
 
- })
+
+test('Creator deletes their blog', async ({ page }) => {
+  await page.getByRole('textbox').first().fill('mluukkai')
+  await page.getByRole('textbox').last().fill('salainen')
+  await page.getByRole('button', { name: 'login' }).click()
+
+  await page.getByRole('button', { name: 'add blog' }).click()
+  await page.getByTestId('Title').fill('DeleteThis')
+  await page.getByTestId('Author').fill('Rulebreaker!')
+  await page.getByTestId('URL').fill('http://delete.com')
+  await page.getByRole('button', { name: 'Create' }).click()
+
+  await page.getByText('DeleteThis Rulebreaker!').getByRole('button', { name: 'view' }).click()
+
+  page.once('dialog', async dialog => {
+    expect(dialog.message()).toContain('Delete "DeleteThis"')
+    await dialog.accept()
+  })
+
+  await page.getByRole('button', { name: 'Remove' }).click()
+
+  await expect(page.getByText('DeleteThis Rulebreaker!')).not.toBeVisible()
+})
+
+
+test('Blog maker sees delete', async ({ page, request }) => {
+ 
+  await page.getByRole('textbox').first().fill('mluukkai')
+  await page.getByRole('textbox').last().fill('salainen')
+  await page.getByRole('button', { name: 'login' }).click()
+
+  await page.getByRole('button', { name: 'add blog' }).click()
+  await page.getByTestId('Title').fill('My Blog')
+  await page.getByTestId('Author').fill('mluukkai')
+  await page.getByTestId('URL').fill('http://ehm.com')
+  await page.getByRole('button', { name: 'Create' }).click()
+  await page.getByRole('button', { name: 'Logout' }).click()
+
+  await page.getByRole('textbox').first().fill('Johnny')
+  await page.getByRole('textbox').last().fill('secret!')
+  await page.getByRole('button', { name: 'login' }).click()
+  await expect(page.getByText('My Blog mluukkai')).toBeVisible()
+  await page.getByText('My Blog mluukkai').getByRole('button', { name: 'view' }).click()
+
+  await expect(page.getByRole('button', { name: 'Remove' })).toHaveCount(0)
+})
+  })
