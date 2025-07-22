a9dbe247738fdf0aafbdd73819808357eb47c34b
Mellemy <emppuli2003@gmail.com>
2025-07-22 04:07:15 +0300

    8.8 ja 8.9 done

--- osa8/library-frontend/src/components/Authors.jsx
+++ osa8/library-frontend/src/components/Authors.jsx
@@ -1,8 +1,24 @@
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
+
 const Authors = (props) => {
   if (!props.show) {
     return null
   }
-  const authors = []
+  const result = useQuery(ALL_AUTHORS)
+  if ( result.loading ) {
+  return <div>loading...</div>
+}
+   const authors = result.data.allAuthors
 
   return (
     <div>
