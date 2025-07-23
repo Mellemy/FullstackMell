aaba7b35dba4e0f627c94203dadca69299e66102
Mellemy <emppuli2003@gmail.com>
2025-07-23 21:12:08 +0300

    8.17 done

--- osa8/library-frontend/src/App.jsx
+++ osa8/library-frontend/src/App.jsx
@@ -3,40 +3,14 @@ import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import BirthYear from "./components/BirthYear"
-const client = useApolloClient()
+import { useApolloClient } from '@apollo/client'
 
 const App = () => {
+  const client = useApolloClient()
   const [token, setToken] = useState(null)
   const [page, setPage] = useState("authors");
 
-if (!token) {
-    return (
-      <div>
-        <Notify errorMessage={errorMessage} />
-        <h2>Login</h2>
-        <LoginForm
-          setToken={setToken}
-          setError={notify}
-        />
-      </div>
-    )
-  }
 
-  const logout = () => {
-    setToken(null)
-    localStorage.clear()
-    client.resetStore()
-  }
-
-  if (!token) {
-    return (
-      <>
-        <Notify errorMessage={errorMessage} />
-        <LoginForm setToken={setToken} setError={notify} />
-      </>
-    )
-  }
-  
   return (
     <div>
       <div>
@@ -44,7 +18,7 @@ if (!token) {
         <button onClick={() => setPage("books")}>books</button>
         <button onClick={() => setPage("add")}>add book</button>
          <button onClick={() => setPage("update")}>edit birth year</button>
-         <button onClick={logout}>logout</button>
+    
       </div>
 
       <Authors show={page === "authors"} />
